090fe5947d471dfc2411ed1cd39bd09978f63acb
Emmanuel <emmanuel.mukira@student.moringaschool.com>
2023-06-02 10:16:51 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < Sinatra::Base
-
-  get '/' do
-    { message: "Hello world" }.to_json
-  end
-
+  set :default_content_type, 'application/json'
+ 
+  get '/games/:id/reviews_with_users' do 
+    game = Game.find(params[:id])
+     game.to_json(include: { reviews: { include: :user } }) 
+    end
+    
+   
+    
 end
